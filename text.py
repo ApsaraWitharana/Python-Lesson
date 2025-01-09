3a2e59ea691e4d7359c69b073b054df2f7decc3c
ApsaraWitharana <asachini095@gmail.com>
2025-01-09 17:05:29 +0530

    add -last lesson - day 8

--- text.py
+++ text.py
@@ -1,1 +1,12 @@
 print("Hellow")
+#error handle
+
+# import pdb;pdb.set_trace()
+breakpoint()
+def multiplication(a,b):
+    answer = a * b
+    return answer
+pdb.set_trace()
+x = input("Enter first Number:")
+y = input("Enter second number:")
+mul = multiplication(x,y)
